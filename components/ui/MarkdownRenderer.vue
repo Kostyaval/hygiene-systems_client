5dd27c536803ba2e71df50ada9a07f56793c9e26
Konstantin Rebenko <rebenkokk@gmail.com>
2023-05-08 19:13:40 +0300

    feat(ui): create MarkdownRenderer component based on snarkdown module

---
+++ components/ui/MarkdownRenderer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-html="md(props.markdown)" />
+</template>
+
+<script setup>
+import md from 'snarkdown'
+
+const props = defineProps({
+  markdown: {
+    type: String,
+    default: '',
+  },
+})
+</script>
