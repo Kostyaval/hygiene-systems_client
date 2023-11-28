f8a3e3856085cf18580d8b35782f15785502930f
Konstantin Rebenko <rebenkokk@gmail.com>
2023-11-28 23:50:52 +0200

    feat(app): add sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,6 +54,7 @@ export default defineNuxtConfig({
   },
   css: ['@/assets/styles/global.sass'],
   modules: [
+    'nuxt-simple-sitemap',
     'nuxt-font-loader',
     '@nuxtjs/tailwindcss',
     '@nuxtjs/svg-sprite',
